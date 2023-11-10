f652e71d894d6228d312fa4b62c6a73ad37c80bf
Farid Khan <farid41658@gmail.com>
2023-11-10 00:25:04 +0000

    added delete function

--- src/BlogTemplate.js
+++ src/BlogTemplate.js
@@ -1,11 +1,11 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "./firebaseconfig";
 import { Link } from "react-router-dom";
 
 const BlogTemplate = () => {
     
-    const [allblogs, setAllblogs] = useState([])
+    const [allblogs, setAllblogs] = useState()
 
     var blogID;
     if(JSON.parse(localStorage.getItem('blogID'))){
@@ -29,10 +29,9 @@ const BlogTemplate = () => {
         const fetchData = async () => {
           try {
             const queryRef = query(collection(db, 'allBlogs'), where('blogID', "==", blogID));
-            getDocs(queryRef)
-            .then((querySnapshot) => {
-                const newBlogs = [];
-                querySnapshot.forEach((doc) => {
+            const querySnapshot = await getDocs(queryRef);
+            if (!querySnapshot.empty) {
+              const doc = querySnapshot.docs[0];
                     const blogDetails = {
                         title: doc.data().title,
                         post: doc.data().post,
@@ -40,20 +39,26 @@ const BlogTemplate = () => {
                         date: doc.data().dateAdded,
                         time: doc.data().timeAdded,
                         blogID: doc.data().blogID
-                    }
-                    newBlogs.push(blogDetails);
-                    });
-                    setAllblogs(newBlogs);
-            })
+                      };
+                    setAllblogs(blogDetails);
+            } else{
+              console.log("No blog found with the specified blogID");
+
+            }
 
           } catch (error) {
             console.error("Error fetching data:", error);
           }
         };
     
-        // Call the async function
-        fetchData();}
+        fetchData();
+      }
       }, [blogID]);
+      
+
+      const deleteBlog = async (blogID) => {
+        await deleteDoc(doc(db, "allBlogs", blogID));
+      }
 
 
 
@@ -61,15 +66,15 @@ const BlogTemplate = () => {
         <div>
              
             <div className="flex justify-start m-5">
-                {allblogs.map((blog, index) => (
-                    <div key={index} className=''>
-                            <h1 className="text-3xl pb-4">{blog.title}</h1>
-                            <p className="text-2xl pb-5">Post: {blog.post}</p>
-                            <p className="text-sm">Written by: <b>{blog.author}</b></p>
-                            <p className="text-sm">Date Added: <b>{blog.date}</b></p>
-                        
-                    </div>
-                ))}
+ 
+            {allblogs && (
+              <div>
+                <h1 className="text-3xl pb-4">{allblogs.title}</h1>
+                <p className="text-2xl pb-5">Post: {allblogs.post}</p>
+                <p className="text-sm">Written by: <b>{allblogs.author}</b></p>
+                <p className="text-sm">Date Added: <b>{allblogs.date}</b></p>
+              </div>
+      )}
             </div>
                     
             <button className="py-2 px-5 m-2 text-l
@@ -77,7 +82,7 @@ const BlogTemplate = () => {
                     hover:scale-110 duration-300" >Publish</button>
             <button className="py-2 px-5 m-2 text-l
                     border-2 border-black rounded-xl hover:bg-slate-300 
-                    hover:scale-110 duration-300" >Delete</button>
+                    hover:scale-110 duration-300" onClick={()=>deleteBlog(blogID)}><Link to={'/ownblogs'}>Delete</Link></button>
             <button className="py-2 px-5 m-2 text-l
                     border-2 border-black rounded-xl hover:bg-slate-300 
                     hover:scale-110 duration-300"><Link to='/ownblogs'>Go Back</Link></button>
